d4e4b308867e20c326da7a15ec7d49d68d0d35e3
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2020-10-25 21:49:48 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # javascriptchallenge
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+GIVEN: the user needs a new, secure password
+The user clicks the button to generate a password 
+The user is presented presented with a series of prompts for password criteria.
+The first prompt asks to pick how many characters long I would like the password to be (between 8 and 128)
+upon entering a number I get a second prompt
+The second prompt asks if the password desired will have upper case letters
+There are two options "ok" and "cancel" 
+the user can select either "ok" to accept the criteria or "cancel" to not accept the criteria 
+either way the next prompt will ask if the user wants lower case letters
+There are two options "ok" and "cancel" 
+the user can select either "ok" to accept the criteria or "cancel" to not accept the criteria 
+either way the next prompt will ask if the user wants "special characters" in the password 
+There are two options "ok" and "cancel" 
+the user can select either "ok" to accept the criteria or "cancel" to not accept the criteria 
+either way the next prompt will ask if the user wants "numbers" in the password 
+There are two options "ok" and "cancel" 
+the user can select either "ok" to accept the criteria or "cancel" to not accept the criteria 
+after these four prompts and based on the users accepted criteria the password will generate
+the password will generate in the box above the "generate password" button
+
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Assignment code here
 
-// global variables
-
 
 
 // string variables
@@ -9,8 +7,6 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuv";
 var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var numbers = "1234567890";
-var reg = /^\d+$/;
-
 
 
 
@@ -27,7 +23,7 @@ function generatePassword() {
     var length = window.prompt("How many characters long do you want your password to be? (between 8 and 128)");
 
     //check for appropiate length, if true, ask if user wants uppercase letters in password
-    while(length < 8 || length >= 129 || !reg.test(length)) {
+    while(length <= 8 && length >= 128) {
     
 
 
@@ -36,11 +32,13 @@ function generatePassword() {
         length = window.prompt("How many characters long do you want your password to be? (between 8 and 128)");
     }
     up = window.confirm("Do you want upper case letters in your password?");
-    //if user wants uppercase letters add them to their userChoice
+    //if user wants uppercase letters, add them to their userChoice
     if(up) {
         
         userChoice = userChoice + upperCase;
     }
+
+    //if user wants lowercase letters, add them to their userChoice
 
     low = window.confirm("Do you want lower case letters in your password?");
 
@@ -50,11 +48,15 @@ function generatePassword() {
 
     }
 
+    //if user wants special characters, add them to their userChoice
+
     spec = window.confirm("Do you want special characters in your password?")
 
     if(spec) {
         userChoice = userChoice + specialChar
     }
+
+    //if user wants numbers, add them to their userChoice
 
     num = window.confirm("Do you want numbers in your password?")
 
@@ -62,25 +64,14 @@ function generatePassword() {
         userChoice = userChoice + numbers
     }
 
-     
-
-    
-
-    //confirm if: upper case, 
-    
-    
-    //lower case, 
-    
-    //special characters
-    // validate on these and make sure they choose at least one type
-
     // I. take their answers and spit out a random password
     // a. think about if statements, arrays (or strings) to hold all characters, and a for loop, and math.random
-
     var password = "";
     for (var i = 0; i < length; i++) {
         password += userChoice[Math.floor(Math.random() * userChoice.length)];
     } 
+
+    //verify the password generated
     var verfied = {
         up: !up,
         low: !low,
@@ -107,7 +98,7 @@ function generatePassword() {
     }
 
     for (let type in verfied) {
-        if (verfied[type] === false) {
+        if (verfied[type] == false) {
             return generatePassword();
         }
     }
